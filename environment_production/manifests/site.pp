6d78ec0194c381a5973b85c19b72dd892308496f
Chris Gascoigne <cgascoig@cisco.com>
2016-03-04 17:26:40 +1100

    more puppet examples

--- environment_production/manifests/site.pp
+++ environment_production/manifests/site.pp
@@ -11,11 +11,17 @@ node /^n9kv\d/ {
 	}
 
 	syslog_server {'10.67.28.129':
-		ensure  => absent,
+		ensure  => present,
 	}
 
-	syslog_server {'10.67.28.130':
-		ensure  => present,
+	cisco_ospf {'underlay':
+		ensure	=> present,
+	}
+
+	cisco_interface {"Ethernet1/2":
+ 		ensure => present,
+		description => "server-web",
+		access_vlan	=>	100,
 	}
 
 }
